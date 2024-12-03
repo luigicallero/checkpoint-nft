c7d7381def704b66373f4405f9e5aa6bcc604606
luigicallero <luigicallero@gmail.com>
2024-12-03 20:40:18 +1300

    updating contract

--- src/CheckPointNFT.sol
+++ src/CheckPointNFT.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract CheckPointNFT is ERC721, Ownable {
+    using Strings for uint256;
     uint256 private _nextTokenId;
     string private _baseTokenURI;
 
@@ -20,6 +23,7 @@ contract CheckPointNFT is ERC721, Ownable {
         uint256 timePlayed;
         uint256 kills;
         string[] boosters;
+        string imageURI;
     }
 
     // Mapping from token ID to checkpoint data
@@ -49,7 +53,8 @@ contract CheckPointNFT is ERC721, Ownable {
         string[] memory weapons,
         uint256 timePlayed,
         uint256 kills,
-        string[] memory boosters
+        string[] memory boosters,
+        string memory imageURI
     ) external returns (uint256) {
         require(authorizedWorlds[msg.sender], "Only authorized worlds can mint checkpoints");
         
@@ -66,7 +71,8 @@ contract CheckPointNFT is ERC721, Ownable {
             weapons: weapons,
             timePlayed: timePlayed,
             kills: kills,
-            boosters: boosters
+            boosters: boosters,
+            imageURI: imageURI
         });
 
         return tokenId;
@@ -83,5 +89,36 @@ contract CheckPointNFT is ERC721, Ownable {
 
     function setBaseURI(string memory newBaseURI) external onlyOwner {
         _baseTokenURI = newBaseURI;
+    }
+
+    function updateCheckpointImage(uint256 tokenId, string memory newImageURI) external {
+        require(msg.sender == ownerOf(tokenId), "Only token owner can update image");
+        checkpoints[tokenId].imageURI = newImageURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_ownerOf(tokenId) != address(0), "Token does not exist");
+        CheckpointData memory checkpoint = checkpoints[tokenId];
+        
+        return string(abi.encodePacked(
+            'data:application/json;base64,',
+            Base64.encode(bytes(abi.encodePacked(
+                '{"name": "Checkpoint #', 
+                tokenId.toString(),
+                '", "description": "Game checkpoint in ', 
+                checkpoint.worldName,
+                '", "image": "', 
+                checkpoint.imageURI,
+                '", "attributes": [',
+                    '{"trait_type": "Level", "value": ', checkpoint.levelNumber.toString(), '},',
+                    '{"trait_type": "Progress", "value": ', checkpoint.levelPercentage.toString(), '},',
+                    '{"trait_type": "Score", "value": ', checkpoint.playerScore.toString(), '},',
+                    '{"trait_type": "Health", "value": ', checkpoint.health.toString(), '},',
+                    '{"trait_type": "Shield", "value": ', checkpoint.shield.toString(), '},',
+                    '{"trait_type": "Time Played", "value": ', checkpoint.timePlayed.toString(), '},',
+                    '{"trait_type": "Kills", "value": ', checkpoint.kills.toString(), '}',
+                ']}'
+            )))
+        ));
     }
 }
